ebfd231a4412996306a711ece78199b6d352bab5
beefchimi <curtdulmage@gmail.com>
2015-03-31 16:49:25 -0400

    initial commit

---
+++ dev/scripts/scripts.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+	// Global Variables
+	// ----------------------------------------------------------------------------
+	var elHTML            = document.documentElement,
+		elBody            = document.body,
+		transitionEvent   = whichTransitionEvent(),
+		animationEvent    = whichAnimationEvent();
+
+
+	// Helper: Check when a CSS transition or animation has ended
+	// ----------------------------------------------------------------------------
+	function whichTransitionEvent() {
+
+		var trans,
+			element     = document.createElement('fakeelement'),
+			transitions = {
+				'transition'       : 'transitionend',
+				'OTransition'      : 'oTransitionEnd',
+				'MozTransition'    : 'transitionend',
+				'WebkitTransition' : 'webkitTransitionEnd'
+			}
+
+		for (trans in transitions) {
+			if (element.style[trans] !== undefined) {
+				return transitions[trans];
+			}
+		}
+
+	}
+
+	function whichAnimationEvent() {
+
+		var anim,
+			element    = document.createElement('fakeelement'),
+			animations = {
+				'animation'       : 'animationend',
+				'OAnimation'      : 'oAnimationEnd',
+				'MozAnimation'    : 'animationend',
+				'WebkitAnimation' : 'webkitAnimationEnd'
+			}
+
+		for (anim in animations) {
+			if (element.style[anim] !== undefined) {
+				return animations[anim];
+			}
+		}
+
+	}
+
+
+	// Initialize Primary Functions
+	// ----------------------------------------------------------------------------
+	// pageLoaded();
+
+
+});
